e82891c4d75d3524b5fce50bd3eb75d6cde18dcc
SikahevaEA <144147492+SikahevaEA@users.noreply.github.com>
2023-10-02 23:33:42 +0500

    Add files via upload

---
+++ Tiuiu.SikachevaEA.Sprint1.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tiuiu.SikachevaEA.Sprint1.Task3.V5.Lib;
+
+namespace Tiuiu.SikachevaEA.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression1()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 7;
+            int r = 35;
+            var res = ds.DistanceLength(x, y);
+            Assert.AreEqual(r, res);
+
+        }
+    }
+}
---
+++ Tiuiu.SikachevaEA.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tiuiu.SikachevaEA.Sprint1.Task3.V5.Lib;
+
+//Написать программу, которая запрашивает у пользователя исходные данные,
+//выполняет указанные расчёты и печатает результат на экране.
+
+namespace Tiuiu.SikachevaEA.Sprint1.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int x = 120;
+            Console.WriteLine("Масштаб карты = " + x + "(количество км. в одном см.)");
+            double y = 3.5;
+            Console.WriteLine("Расстояние между точками, изображающими населенные пункты = " + y + "см");
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Расстояние между населенными пунктами = " + Math.Round(ds.DistanceLength(x, y), 3));
+            Console.ReadKey();
+        }
+    }
+}
